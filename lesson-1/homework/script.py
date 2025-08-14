7c7926807378541abbc461d7f53d7e7208999a94
Rustam Ruzayev <rustamruzayev1989@gmail.com>
2025-08-14 19:57:16 +0500

    Update script.py

--- lesson-1/homework/script.py
+++ lesson-1/homework/script.py
@@ -3,3 +3,17 @@ perimetr = a * 4
 s = a ** 2
 print(perimetr)
 print(s)
+D = 10
+π = 3.14
+c = D * π
+print(round(c))
+a = 10
+b = 6
+c = (a+b)/2
+print(c)
+a = 2
+b = 4
+print(a+b)
+print(a*b)
+print(a**2)
+print(b**2)
