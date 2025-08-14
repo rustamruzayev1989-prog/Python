d4bdbc176449b5e008a368ad73dc04fff05f28c7
Rustam Ruzayev <rustamruzayev1989@gmail.com>
2025-08-14 20:09:13 +0500

    Update script.py

--- lesson-3/homework/script.py
+++ lesson-3/homework/script.py
@@ -11,3 +11,6 @@ if "Parij" in shaharlar:
     print("Parij ro'yxatda bor!")
 else:
     print("Parij ro'yxatda yo'q!")
+numbers = [1, 2, 3, 4, 5]
+doubled_numbers = numbers * 2
+print(doubled_numbers)
