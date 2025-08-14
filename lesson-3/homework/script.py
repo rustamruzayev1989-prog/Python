c945af8340a7e504eb2cf5287ca09cf77760c4c5
Rustam Ruzayev <rustamruzayev1989@gmail.com>
2025-08-14 20:08:51 +0500

    Update script.py

--- lesson-3/homework/script.py
+++ lesson-3/homework/script.py
@@ -6,3 +6,8 @@ print(list1+list2)
 favorite_movies=("Terminator","The Matrix", "Inception", "Interstellar", "The Martian")
 movies_tuple = tuple(favorite_movies)
 print(favorite_movies)
+shaharlar = ["London", "Parij", "Istaambul", "Moskva", "Praga"]
+if "Parij" in shaharlar:
+    print("Parij ro'yxatda bor!")
+else:
+    print("Parij ro'yxatda yo'q!")
