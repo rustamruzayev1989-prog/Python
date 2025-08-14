7efe931a7067ba6847abdd95f1661d42a861ce23
Rustam Ruzayev <rustamruzayev1989@gmail.com>
2025-08-14 20:00:10 +0500

    Update script.py

--- lesson-2/homework/script.py
+++ lesson-2/homework/script.py
@@ -1,1 +1,6 @@
-
+from datetime import datetime
+name = input("Iltimos, ismingni kirit: ")
+year_of_birth = int(input("Iltimos, tugilgan yilingni kirit: "))
+current_year = datetime.now().year
+age = current_year - year_of_birth
+print(f"Salom, {name}! Sen {age} yoshdasan.")
