e3e9864104c8045adb7eadd9e2aff7c5eabf0b30
Rustam Ruzayev <rustamruzayev1989@gmail.com>
2025-08-14 20:08:15 +0500

    Update script.py

--- lesson-3/homework/script.py
+++ lesson-3/homework/script.py
@@ -3,3 +3,6 @@ print(mevalar[2])
 list1=[1, 2, 3]
 list2=[4, 5, 6]
 print(list1+list2)
+favorite_movies=("Terminator","The Matrix", "Inception", "Interstellar", "The Martian")
+movies_tuple = tuple(favorite_movies)
+print(favorite_movies)
