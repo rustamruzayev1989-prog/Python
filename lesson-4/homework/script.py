d61425d098b95fc84e1a0b1ea0265e62719cb1cf
Rustam Ruzayev <rustamruzayev1989@gmail.com>
2025-08-19 15:45:59 +0500

    Update script.py

--- lesson-4/homework/script.py
+++ lesson-4/homework/script.py
@@ -1,3 +1,12 @@
+my_dict = {"olma": 2, "olcha": 1,"anor": 4, "behi": 3}
+sorted_dict = sorted((my_dict.items()),key=lambda x: x[1])
+print(sorted_dict)
+my_dict = {"olma": 2, "olcha": 1,"anor": 4, "behi": 3}
+sorted_dict = sorted((my_dict.items()),key=lambda x: x[1], reverse=True)
+print(sorted_dict)
+
+
+
 dic1 = {1: 10, 2: 20}
 dic2 = {3: 30, 4: 40}
 dic3 = {5: 50, 6: 60}
