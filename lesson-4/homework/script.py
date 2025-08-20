5b4f7fdfcd3858bd6169d7a937e47d69eca32bf7
Rustam Ruzayev <rustamruzayev1989@gmail.com>
2025-08-20 17:14:23 +0500

    Update script.py

--- lesson-4/homework/script.py
+++ lesson-4/homework/script.py
@@ -18,3 +18,7 @@ for i in range(1, n+1):
     my_dict[i] = i*i
 print(f"n uchun lug'at n={n}:")
 print(my_dict)
+my_dict = {}
+for i in range(1, 16):
+    my_dict[i] = i*i
+print("my_dict:", my_dict)
