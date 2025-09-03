d02f41bc39d3b2638d20e87cee59b0d3c5364d1c
Rustam Ruzayev <rustamruzayev1989@gmail.com>
2025-09-03 15:40:24 +0500

    Update script.py

--- lesson5/homework/script.py
+++ lesson5/homework/script.py
@@ -24,3 +24,16 @@ check_number(3)
 check_number(4)   
 check_number(18)  
 check_number(22) 
+def even_numbers_with_if(a, b):
+    if a % 2 == 0:
+        start = a
+    else:
+        start = a + 1
+    return list(range(start, b + 1, 2))
+print(even_numbers_with_if(3, 10))  
+print(even_numbers_with_if(2, 7))   
+def even_numbers_no_if(a, b):
+    start = a + (a % 2)  
+    return list(range(start, b + 1, 2))
+print(even_numbers_no_if(3, 10))  
+print(even_numbers_no_if(2, 7))  
