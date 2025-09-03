c8840d88893efcf5b0ede3e3229745a140b77072
Rustam Ruzayev <rustamruzayev1989@gmail.com>
2025-09-03 15:29:22 +0500

    Update script.py

--- lesson7/homework/script.py
+++ lesson7/homework/script.py
@@ -11,3 +11,9 @@ def digit_sum(k):
     return sum(int(digit) for digit in str(k))
 print(digit_sum(24)) 
 print(digit_sum(502))
+def powers_of_two(N):
+    k = 1
+    while 2 ** k <= N:
+        print(2 ** k, end=" ")
+        k += 1  
+powers_of_two(10)
