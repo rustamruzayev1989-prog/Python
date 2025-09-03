ce62e36cb06bf074d18270b2374703c25b0451db
Rustam Ruzayev <rustamruzayev1989@gmail.com>
2025-09-03 15:34:29 +0500

    Update script.py

--- lesson5/homework/script.py
+++ lesson5/homework/script.py
@@ -1,1 +1,13 @@
-
+year = int(input("year="))
+if(year%4==0):
+    print("leap year")
+else:
+    print("not leap year")
+    if(year%100==0):
+        print("leap year")
+    else:
+        print("not leap year")
+        if(year%400==0):
+            print("leap year")
+        else:
+            print("not leap year")
