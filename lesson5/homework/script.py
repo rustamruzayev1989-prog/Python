5c81738110e7b767df2fa83f3420f8328e562f6a
Rustam Ruzayev <rustamruzayev1989@gmail.com>
2025-09-03 15:36:48 +0500

    Update script.py

--- lesson5/homework/script.py
+++ lesson5/homework/script.py
@@ -11,3 +11,16 @@ else:
             print("leap year")
         else:
             print("not leap year")
+def check_number(n):
+    if n % 2 == 1:  
+        print("Weird")
+    elif 2 <= n <= 5:
+        print("Not Weird")
+    elif 6 <= n <= 20:
+        print("Weird")
+    else:  
+        print("Not Weird")
+check_number(3)   
+check_number(4)   
+check_number(18)  
+check_number(22) 
