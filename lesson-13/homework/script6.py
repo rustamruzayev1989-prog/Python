3ba7719f4d23f4e4f05dd439c63c12bba44b5ff7
Rustam Ruzayev <rustamruzayev1989@gmail.com>
2025-09-10 15:04:18 +0500

    Create script6.py

---
+++ lesson-13/homework/script6.py
@@ -0,0 +1,17 @@
+import re
+
+def validate_email(email: str) -> bool:
+   
+    pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    return re.match(pattern, email) is not None
+
+def main():
+    email = input("Введите адрес электронной почты: ").strip()
+
+    if validate_email(email):
+        print("✅ Email корректный!")
+    else:
+        print("Неверный формат email!")
+
+if __name__ == "__main__":
+    main()
