8e4924c9fdb8ef93f175be60759b8008e2080a92
Rustam Ruzayev <rustamruzayev1989@gmail.com>
2025-09-10 10:47:17 +0500

    Update script4.py

--- homework/lesson8/script4.py
+++ homework/lesson8/script4.py
@@ -1,10 +1,15 @@
 try:
     a = input("Enter first number: ")
     b = input("Enter second number: ")
-      if not (a.isdigit() and b.isdigit()):
+
+    
+    if not (a.isdigit() and b.isdigit()):
         raise TypeError("Inputs must be numbers")
+
     a = int(a)
     b = int(b)
+
     print("Sum:", a + b)
+
 except TypeError as e:
     print("TypeError:", e)
