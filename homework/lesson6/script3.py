8654db231e779bb115df2f319676236c67f37229
Rustam Ruzayev <rustamruzayev1989@gmail.com>
2025-09-10 09:37:27 +0500

    Update script3.py

--- homework/lesson6/script3.py
+++ homework/lesson6/script3.py
@@ -7,3 +7,10 @@ for i in range(1, n + 1):
     for j in range(1, i + 1):
         print(j, end=" ")
     print()
+n = int(input("Enter number "))
+total = 0
+i = 1
+while i <= n:
+    total += i
+    i += 1
+print("Sum is:", total)
