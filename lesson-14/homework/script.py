a5391ce0df11789db1c88ea7ee4067443d60aaa8
Rustam Ruzayev <rustamruzayev1989@gmail.com>
2025-09-12 20:14:32 +0500

    Update script.py

--- lesson-14/homework/script.py
+++ lesson-14/homework/script.py
@@ -1,4 +1,13 @@
+#Анализ JSON (чтение students.json)
 [
   {"name": "Alice", "age": 20, "grade": "A"},
   {"name": "Bob",   "age": 22, "grade": "B"}
 ]
+import json
+
+with open("students.json", "r", encoding="utf-8") as f:
+    students = json.load(f)
+
+print("Сведения о студентах:")
+for s in students:
+    print(f"Имя: {s['name']}, Возраст: {s['age']}, Оценка: {s['grade']}")
