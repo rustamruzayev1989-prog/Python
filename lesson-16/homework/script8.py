33bf0e61faaedaac2613f6b72ba859d56a97ce98
Rustam Ruzayev <rustamruzayev1989@gmail.com>
2025-09-12 20:29:15 +0500

    Update script8.py

--- lesson-16/homework/script8.py
+++ lesson-16/homework/script8.py
@@ -1,16 +1,17 @@
 import numpy as np
 
+
+np.random.seed(0)
+
+
 arr = np.random.randint(0, 101, size=10)
 print("Случайный массив:", arr)
 
-mean_val = np.mean(arr)
-median_val = np.median(arr)
-std_val = np.std(arr)
-min_val = np.min(arr)
-max_val = np.max(arr)
+
+mean_val = np.mean(arr)      # среднее значение
+median_val = np.median(arr)  # медиана
+std_val = np.std(arr)        # стандартное отклонение
 
 print("Среднее значение:", mean_val)
 print("Медиана:", median_val)
 print("Стандартное отклонение:", std_val)
-print("Минимум:", min_val)
-print("Максимум:", max_val)
